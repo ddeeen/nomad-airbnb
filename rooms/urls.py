644334633a4355815c255160aa7eded5147d2119
Sumin <naenjo0315@naver.com>
2024-10-06 00:22:40 +0900

    amenities api setting

--- rooms/urls.py
+++ rooms/urls.py
@@ -1,1 +1,7 @@
-urlpatterns = []
+from rest_framework.urls import path
+from . import views
+
+urlpatterns = [
+    path("amenities/", views.Amenities.as_view()),
+    path("amenity/<int:pk>", views.AmenityDetail.as_view()),
+]
--- rooms/views.py
+++ rooms/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import Amenity
+from .serializers import AmenitySerializer
+
 
 # Create your views here.
+class Amenities(APIView):
+
+    def get(self, request):
+        amenities = Amenity.objects.all()
+        serializer = AmenitySerializer(amenities, many=True)
+        return Response(serializer.data)
+
+    def put(self, request):
+        serializer = AmenitySerializer(data=request.data)
+        if serializer.is_valid():
+            amenity = serializer.save()
+            return Response(AmenitySerializer(amenity).data)
+        else:
+            return Response(serializer.errors)
+
+
+class AmenityDetail(APIView):
+
+    def get(self, request):
+        pass
+
+    def put(self, request):
+        pass
+
+    def delete(self, request):
+        pass
